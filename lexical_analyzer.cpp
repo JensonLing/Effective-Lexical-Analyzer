f327bebc62010690faeb6bf22c83b5927478ad6a
jerison <lingjie981119@126.com>
2020-10-23 03:28:18 +0800

    v1.1

--- lexical_analyzer.cpp
+++ lexical_analyzer.cpp
@@ -8,7 +8,9 @@
 
 using namespace std;
 
-map<string,int> tokens;
+map<string,int> tokens;//包含所有可识别的符号，可添加
+map<int,string> token_type; //数字所代表的符号类型，固定
+map<int,int> token_num;
 queue<char> token_queue;
 
 void read_op_table(string path)
@@ -18,10 +20,14 @@ void read_op_table(string path)
     string out;
     while(t.getline(temp,1024))
     {
-        out.append(temp);
+        int type = atoi(temp);
+        t.getline(temp,1024);
+        string s = temp;
+        tokens[s] = type;
+         //cout<<s.length();
+
         //out.append("\0");
     }
-    cout<<out[1]<<endl;
     t.close();
 }
 
@@ -40,6 +46,8 @@ int judge_type(char c)
         return tokens[temp_c];
     else if(c == EOF)
         return -1;
+    else if(c == '\n')
+        return -4;
     else
         return -2;
 }
@@ -48,13 +56,14 @@ void analyze(char* s)
 {
     int i = 0;
     int state = judge_type(s[i]);
-    while(s[i] != 'z')
+    bool over = false;
+    while(!over)
     {
         //state = judge_type(s[i]);
         //cout<<"in_while";
         switch(state)
         {
-            case 0:
+            case 0://空格，无需输出
                 i++;
                 state = judge_type(s[i]);
                 break;
@@ -78,10 +87,78 @@ void analyze(char* s)
                     state = judge_type(s[i]);
                 }
                 break;
+            case 2:
+                //cout<<"2->"<<"pushed:"<<s[i]<<'~';
+                token_queue.push(s[i]);
+                //cout<<"front:"<<token_queue.front()<<'~';
+                i++;
+                if(judge_type(s[i]) == 2)
+                    state = 2;
+                else if(s[i] == '.')
+                {
+                    token_queue.push(s[i]);
+                    //cout<<"pushed:"<<s[i]<<'~';
+                    //cout<<"front:"<<token_queue.front()<<'~';
+                    i++;
+                    state = 3;
+                }
+                else if(s[i] == 'e')
+                {
+                    token_queue.push(s[i]);
+                    i++;
+                    state = 5;
+                }
+                break;
             case 3:
+                //cout<<"3->";
+                if(judge_type(s[i]) == 2)
+                {
+                    token_queue.push(s[i]);
+                    i++;
+                    state = 4;
+                }
+                else
+                {
+                    token_queue.push('0');
+                    state = judge_type(s[i]);
+                    while(!token_queue.empty())
+                    {
+                        //cout<<"pop:";
+                        cout<<token_queue.front();
+                        token_queue.pop();
+                    }
+                    cout<<" ";
+                }
+                break;
+            case 4:
+                if(judge_type(s[i]) == 2)
+                {
+                    token_queue.push(s[i]);
+                    i++;
+                    state = 4;//问题：读到换行符就不读了，没来得及pop(已解决)
+                }
+                else
+                {
+                    state = judge_type(s[i]);
+                    while(!token_queue.empty())
+                    {
+                        //cout<<"pop:";
+                        cout<<token_queue.front();
+                        token_queue.pop();
+                    }
+                    cout<<" ";
+                    //state = -4;
+                }
+                break;
+                
+            case -4:
+                over = true;
+                break;
+
+            case -3:
                 i++;
                 state = judge_type(s[i]);
-                cout<<"ERR";
+                cout<<"(ERR) ";
                 break;
         }
     }
@@ -89,9 +166,9 @@ void analyze(char* s)
 
 int main()
 {
-    char s[] = "abc ef&d sda __kjs z";
-    tokens["&"] = 3;
-
+    char s[] = "3. 3.85\n";
+    
+    //tokens["&"] = 3;
     analyze(s);
     //read_op_table("./temp.txt");
     /*
@@ -106,5 +183,8 @@ int main()
     for(iter = temp.begin(); iter != temp.end(); iter++)
       cout << iter->first << " " << iter->second <<" "<<lex[iter->first]<< endl;
       */
+    /*map<string, int> ::iterator iter; 
+    for(iter = tokens.begin(); iter != tokens.end(); iter++)
+        cout << iter->first << " " << iter->second << endl;*/
 }
 
