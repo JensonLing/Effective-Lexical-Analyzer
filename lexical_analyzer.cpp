cd82a748db621eb875ae2455a136a095897a168e
jerison <lingjie981119@126.com>
2020-10-23 20:01:06 +0800

    v1.3

--- lexical_analyzer.cpp
+++ lexical_analyzer.cpp
@@ -8,10 +8,15 @@
 
 using namespace std;
 
-map<string,int> tokens;//包含所有可识别的符号，可添加
+map<string,int> tokens;//包含所有可识别的符号及保留字，可添加
 map<int,string> token_type; //数字所代表的符号类型，固定
-map<int,int> token_num;
+map<int,int> token_num;//用以统计各类型记号的出现次数
+map<string, int> operators;
 queue<char> token_queue;
+
+bool header_flag = false;
+bool crossing_flag = false;
+bool annotation = false;
 
 void read_op_table(string path)
 {
@@ -21,9 +26,10 @@ void read_op_table(string path)
     while(t.getline(temp,1024))
     {
         int type = atoi(temp);
+        //cout<<"get:"<<type<<endl;
         t.getline(temp,1024);
         string s = temp;
-        tokens[s] = type;
+        operators[s] = type;
          //cout<<s.length();
 
         //out.append("\0");
@@ -36,14 +42,17 @@ int judge_type(char c)
     char temp_c[2];
     temp_c[0] =  c;
 
+    if(annotation)
+        return 10;
+
     if(c == ' ')
         return 0;
     else if(c == '_' || (c >= 'a' && c <= 'z') || (c>='A' && c <= 'Z'))
         return 1;
     else if(c>='0' && c<='9')
         return 2;
-    else if(tokens[temp_c])
-        return tokens[temp_c];
+    else if(operators[temp_c])
+        return operators[temp_c];
     else if(c == EOF)
         return -1;
     else if(c == '\n')
@@ -64,10 +73,13 @@ void analyze(char* s)
         switch(state)
         {
             case 0://空格，无需输出
+                cout<<"case0:";
                 i++;
                 state = judge_type(s[i]);
+                //cout<<"next:"<<state;
                 break;
             case 1:
+                cout<<"case1:";
                 token_queue.push(s[i]);
                 //cout<<"pushed:"<<s[i]<<endl;
                 i++;
@@ -85,6 +97,7 @@ void analyze(char* s)
                     }
                     cout<<" ";
                     state = judge_type(s[i]);
+                    cout<<"exit_state:"<<state<<" ";
                 }
                 break;
             case 2:
@@ -143,15 +156,15 @@ void analyze(char* s)
                 }
                 else if(judge_type(s[i]) == 1)
                 {
-                    token_queue.push('0');
-                    state = -6;
+                    //token_queue.push('0');
+                    state = -6;/*
                     while(!token_queue.empty())
                     {
                         //cout<<"pop:";
                         cout<<token_queue.front();
                         token_queue.pop();
                     }
-                    cout<<" ";
+                    cout<<" ";*/
                 }
                 else
                 {
@@ -189,14 +202,14 @@ void analyze(char* s)
                 }
                 else if(judge_type(s[i]) == 1)
                 {
-                    state = -6;
+                    state = -6;/*
                     while(!token_queue.empty())
                     {
                         //cout<<"pop:";
                         cout<<token_queue.front();
                         token_queue.pop();
                     }
-                    cout<<" ";
+                    cout<<" ";*/
                 }
                 else
                 {
@@ -228,7 +241,7 @@ void analyze(char* s)
                 }
                 else if(judge_type(s[i]) == 1)
                 {
-                    state = -6;
+                    state = -6;/*
                     while(!token_queue.empty())
                     {
                         //cout<<"pop:";
@@ -236,7 +249,7 @@ void analyze(char* s)
                             cout<<token_queue.front();
                         token_queue.pop();
                     }
-                    cout<<" ";
+                    cout<<" ";*/
                 }
                 else
                 {
@@ -270,14 +283,14 @@ void analyze(char* s)
                 cout<<" state7:" ;
                 if(judge_type(s[i]) == 1)
                 {
-                    state = -6;
+                    state = -6;/*
                     while(!token_queue.empty())
                     {
                         //cout<<"pop:";
                         cout<<token_queue.front();
                         token_queue.pop();
                     }
-                    cout<<" ";
+                    cout<<" ";*/
                 }
                 else if(judge_type(s[i]) == 2)
                 {
@@ -303,7 +316,107 @@ void analyze(char* s)
                     cout<<" ";
                 }
                 break;
+            case 8://界符
+                if(s[i] == '\'' || s[i] == '\"')
+                {
+                    cout<<"state8:";
+                    cout<<s[i]<<" ";
+
+                    char crossing = s[i];
+                    crossing_flag = true;
+                    i++;
+                    while(crossing_flag && s[i]!='\n')
+                    {
+                        if(s[i] == crossing)
+                        {
+                            crossing_flag = false;
+                            i++;
+                            state = judge_type(s[i]);
+                            while(!token_queue.empty())
+                            {
+                                //cout<<"pop:";
+                                cout<<token_queue.front();
+                                token_queue.pop();
+                            }
+                            cout<<" ";
+                            cout<<crossing<<" ";
+                            
+                        }
+                        else if(s[i+1] == '\n')
+                        {
+                            crossing_flag = false;
+                            token_queue.push(s[i]);
+                            while(!token_queue.empty())
+                            {
+                                //cout<<"pop:";
+                                cout<<token_queue.front();
+                                token_queue.pop();
+                            }
+                            cout<<" ";
+                            over = true;
+                        }
+                        else
+                        {
+                            token_queue.push(s[i]);
+                            i++;
+                        }
+                    }
+                }
+                else
+                {
+                    cout<<"state8:";
+                    cout<<s[i]<<" ";
+                    i++;
+                    state = judge_type(s[i]);
+                }
+                break;
+            case 9://运算符
+                //cout<<"case 9:";
+                if(s[i] == '/')
+                {
+                    i++;
+                    if(s[i] == '*')
+                    {
+                        state = 10;
+                        annotation = true;
+                        i++;
+                    }
+                    else if(s[i] == '/')
+                        over = true;
+
+                }
+                break;
+            case 10://进入注释模式
+                cout<<"case 10:";
+                if(s[i] == '\n')
+                    over = true;
+                else if(s[i] == '*')
+                {
+                    state = 11;
+                    i++;
+                }
+                else
+                {
+                    i++;
+                }
                 
+                break;
+            case 11:
+                cout<<"case 11:";
+                if(s[i] == '/')
+                {
+                    annotation = false;
+                    i++;
+                    state = judge_type(s[i]);
+                }
+                else if(s[i] == '\n')
+                    over = true;
+                else
+                {
+                    state = 10;
+                    i++;
+                }
+                break;
             case -3:
                 i++;
                 state = judge_type(s[i]);
@@ -316,7 +429,7 @@ void analyze(char* s)
                 state = judge_type(s[i]);
                 cout<<"(ERR5) ";
                 break;
-            case -6:
+            case -6://错误，非法标识符
                 token_queue.push(s[i]);
                 i++;
                 if(judge_type(s[i]) == 1)
@@ -333,17 +446,20 @@ void analyze(char* s)
                     cout<<"(ERR0) ";
                 }
                 break;
-                
         }
     }
 }
 
 int main()
 {
-    char s[] = "3. 3. 4. 5eA\n";
-    
+    char s[] = "12as//da int a;\n1234567890982301\n";
     //tokens["&"] = 3;
+    read_op_table("./operator_map.txt");
     analyze(s);
+    cout<<" next state:"<<judge_type(s[16]);
+    cout<<" annotation: "<<annotation;
+    analyze(s+16);
+    //cout<<operators["+"];
     //read_op_table("./temp.txt");
     /*
     map<string,int> temp;
@@ -358,7 +474,7 @@ int main()
       cout << iter->first << " " << iter->second <<" "<<lex[iter->first]<< endl;
       */
     /*map<string, int> ::iterator iter; 
-    for(iter = tokens.begin(); iter != tokens.end(); iter++)
+    for(iter = operators.begin(); iter != operators.end(); iter++)
         cout << iter->first << " " << iter->second << endl;*/
 }
 
